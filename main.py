7bc63d67fbe1937e89ff4cbc02bc3b38dec0df76
caveeroo <62477582+caveeroo@users.noreply.github.com>
2023-11-28 11:17:50 +0100

    Added custom directory and txt/json output
    Tool can now output in .txt or .json the results, as well as specifying directory of CTFd export.
    Improved code for better extensibility.

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+import os
 import json
-from collections import Counter
 import logging
+import argparse
+from collections import Counter
 
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Tool to analyze ctf competitions data")
+    parser.add_argument(
+        '--output', choices=['txt', 'json'], required=True, help="Output format")
+    parser.add_argument('--dir', default='.',
+                        help="Directory containing CTFd export")
+    return parser.parse_args()
 
 
 def load_json_data(file_path):
@@ -16,6 +28,28 @@ def load_json_data(file_path):
     except json.JSONDecodeError as e:
         logging.error(f"JSON Decode Error in {file_path}: {e}")
     return None
+
+
+def output_txt(data):
+    with open('results.txt', 'w') as file:
+        for key, value in data.items():
+            file.write(f"{key}:\n")
+            if isinstance(value, (list, set)):
+                for item in value:
+                    file.write(f"  {item}\n")
+            elif isinstance(value, dict):
+                for subkey, subvalue in value.items():
+                    file.write(f"  {subkey}: {subvalue}\n")
+            else:
+                file.write(f"  {value}\n")
+            file.write("\n")
+    print("saved to results.txt")
+
+
+def output_json(data):
+    with open('results.json', 'w') as file:
+        json.dump(data, file, indent=4)
+    print("saved to results.json")
 
 
 def get_hidden_and_admin_user_ids(users_data):
@@ -39,9 +73,8 @@ def get_challenge_name_mapping(challenges_data):
             for challenge in challenges_data['results']}
 
 
-def submissions_per_category():
-    """Prints the number of submissions per category."""
-    print("\nSubmissions by category:\n")
+def submissions_per_category(submissions_data, hidden_and_admin_user_ids, challenge_category_mapping):
+    """Returns the number of submissions per category."""
     submits_by_category = Counter()
 
     for submission in submissions_data['results']:
@@ -50,15 +83,11 @@ def submissions_per_category():
                 submission['challenge_id'])
             submits_by_category[category] += 1
 
-    for category, count in sorted(
-            submits_by_category.items(), key=lambda item: item[1], reverse=True):
-        print(f"Category {category}: {count} submissions")
+    return sorted(submits_by_category.items(), key=lambda item: item[1], reverse=True)
 
 
-def solves_per_category():
-    """Prints the number of solves per category."""
-    print("\nSolves by category:\n")
-
+def solves_per_category(submissions_data, hidden_and_admin_user_ids, challenge_category_mapping):
+    """Returns the number of solves per category."""
     solves_by_category = Counter()
     for submission in submissions_data['results']:
         if submission['type'] == 'correct' and submission['user_id'] not in hidden_and_admin_user_ids:
@@ -66,61 +95,48 @@ def solves_per_category():
                 submission['challenge_id'])
             solves_by_category[category] += 1
 
-    for category, count in sorted(solves_by_category.items(),
-                                  key=lambda item: item[1], reverse=True):
-        print(f"Category {category}: {count} solves")
+    return sorted(solves_by_category.items(), key=lambda item: item[1], reverse=True)
 
 
-def top_users_failed_submissions_count():
-    """Prints the top 10 users with the highest number of failed submissions."""
-    print("\nTop 10 users with the highest number of failed submissions:\n")
+def top_users_failed_submissions_count(submissions_data, id_to_username, n):
+    """Returns the top N users with the highest number of failed submissions."""
     username_failed_submissions = Counter()
     for submission in submissions_data["results"]:
         if submission["type"] == "incorrect":
             username = id_to_username.get(submission["user_id"], "Unknown")
             username_failed_submissions[username] += 1
 
-    for user, count in username_failed_submissions.most_common(10):
-        print(f"{user}: {count} failed submissions")
+    return username_failed_submissions.most_common(n)
 
 
-def first_bloods():
-    """Prints the first solver of each challenge."""
-    print("\nFirst bloods for each solved challenge:\n")
+def first_bloods(submissions_data, hidden_and_admin_user_ids, id_to_username, challenge_name_mapping):
+    """Returns the first solver of each challenge."""
     first_solvers = {}
 
     for submission in submissions_data['results']:
         if submission['type'] == 'correct' and submission['user_id'] not in hidden_and_admin_user_ids:
             first_solvers.setdefault(
-                submission['challenge_id'],
-                submission['user_id'])
+                submission['challenge_id'], submission['user_id'])
 
-    for challenge_id, user_id in sorted(first_solvers.items(
-    ), key=lambda x: challenge_name_mapping.get(x[0], '')):
-        username = id_to_username.get(user_id, 'Unknown')
-        challenge_name = challenge_name_mapping.get(challenge_id, 'Unknown')
-        print(f"{challenge_name}: {username}")
+    return [(challenge_name_mapping.get(challenge_id, 'Unknown'), id_to_username.get(user_id, 'Unknown'))
+            for challenge_id, user_id in sorted(first_solvers.items(), key=lambda x: challenge_name_mapping.get(x[0], ''))]
 
 
-def challenge_stats():
-    """Prints statistics about solved and unsolved challenges."""
-    print("\nChallenge statistics:\n")
+def challenge_stats(submissions_data, challenges_data):
+    """Returns statistics about solved and unsolved challenges."""
     solved_challenge_ids = {submission['challenge_id']
                             for submission in submissions_data['results'] if submission['type'] == 'correct'}
     all_challenge_ids = {challenge['id']
                          for challenge in challenges_data['results']}
 
-    num_solved_challenges = len(solved_challenge_ids)
-    num_unsolved_challenges = len(all_challenge_ids - solved_challenge_ids)
+    return {
+        "solved_challenges": len(solved_challenge_ids),
+        "unsolved_challenges": len(all_challenge_ids - solved_challenge_ids)
+    }
 
-    print(f"Challenges with at least one solve: {num_solved_challenges}")
-    print(f"Challenges with zero solves: {num_unsolved_challenges}")
 
-
-def most_popular_category():
-    """Prints the most popular category based on solves."""
-    print("\nMost popular category (highest number of solves):")
-
+def most_popular_category(submissions_data, challenge_category_mapping):
+    """Returns the most popular category based on solves."""
     solves_by_category = Counter()
     for submission in submissions_data['results']:
         if submission['type'] == 'correct':
@@ -129,32 +145,28 @@ def most_popular_category():
             solves_by_category[category] += 1
 
     most_popular = max(solves_by_category.items(), key=lambda x: x[1])
-    print(f"\n{most_popular[0]} ({most_popular[1]} solves)")
+    return most_popular
 
 
-def longest_submission():
-    """Prints details of the longest submission."""
-    print("\nLongest submission:\n")
+def longest_submission(submissions_data, id_to_username):
+    """Returns details of the longest submission."""
     longest_submission = max(
-        submissions_data['results'],
-        key=lambda x: len(
-            x['provided']),
-        default=None)
-
+        submissions_data['results'], key=lambda x: len(x['provided']), default=None)
     if longest_submission:
         submission_user = id_to_username.get(
             longest_submission['user_id'], 'Unknown')
-        print(
-            f"The submission with ID {longest_submission['id']} made by user {submission_user} is the longest, with {len(longest_submission['provided'])} characters:\n")
-        print(longest_submission['provided'])
+        return {
+            "id": longest_submission['id'],
+            "user": submission_user,
+            "length": len(longest_submission['provided']),
+            "content": longest_submission['provided']
+        }
     else:
-        print("No submissions found.")
+        return {"message": "No submissions found."}
 
 
-def top_failed_submissions_by_challenge():
-    """Prints challenges with the highest number of failed submissions."""
-    print("\nTop challenges with the highest number of failed submissions:\n")
-
+def top_failed_submissions_by_challenge(submissions_data: dict, hidden_and_admin_user_ids, challenge_name_mapping):
+    """Returns challenges with the highest number of failed submissions."""
     failed_submissions = Counter()
     correct_submissions = Counter()
 
@@ -166,42 +178,86 @@ def top_failed_submissions_by_challenge():
             elif submission['type'] == 'correct':
                 correct_submissions[challenge_id] += 1
 
+    top_challenges = []
     for challenge_id, num_failures in failed_submissions.most_common(3):
         challenge_name = challenge_name_mapping.get(challenge_id, 'Unknown')
         challenge_solves = correct_submissions[challenge_id]
-        solve_rate = round(
-            (challenge_solves / num_failures) * 100, 2) if num_failures else 0
-        print(f"{challenge_name}: {num_failures} failed submissions, {challenge_solves} correct submissions, solve rate: {solve_rate}%")
+        solve_rate = round((challenge_solves / num_failures)
+                           * 100, 2) if num_failures else 0
+        top_challenges.append(
+            (challenge_name, num_failures, challenge_solves, solve_rate))
+
+    return top_challenges
 
 
-def rewind():
+def rewind(data_dir, output_format):
+    submissions_data = load_json_data(
+        os.path.join(data_dir, 'db/submissions.json'))
+    users_data = load_json_data(os.path.join(data_dir, 'db/users.json'))
+    challenges_data = load_json_data(
+        os.path.join(data_dir, 'db/challenges.json'))
 
     if not all([submissions_data, users_data, challenges_data]):
         logging.error("Failed to load data. Exiting.")
         return
 
-    most_popular_category()
-    submissions_per_category()
-    solves_per_category()
-    challenge_stats()
-    first_bloods()
-    top_users_failed_submissions_count()
-    longest_submission()
-    top_failed_submissions_by_challenge()
+    # mappings
+    id_to_username = {user["id"]: user["name"]
+                      for user in users_data.get("results", [])}
+    hidden_user_ids, admin_user_ids = get_hidden_and_admin_user_ids(users_data)
+    hidden_and_admin_user_ids = hidden_user_ids | admin_user_ids
+    challenge_category_mapping = get_challenge_category_mapping(
+        challenges_data)
+    challenge_name_mapping = get_challenge_name_mapping(challenges_data)
+
+    # analysis
+    most_popular_data = most_popular_category(
+        submissions_data, challenge_category_mapping)
+
+    submissions_per_category_data = submissions_per_category(
+        submissions_data, hidden_and_admin_user_ids, challenge_category_mapping)
+
+    solves_data = solves_per_category(
+        submissions_data, hidden_and_admin_user_ids, challenge_category_mapping)
+
+    challenge_stats_data = challenge_stats(submissions_data, challenges_data)
+
+    first_bloods_data = first_bloods(
+        submissions_data, hidden_and_admin_user_ids, id_to_username, challenge_name_mapping)
+
+    top_users_failed_data = top_users_failed_submissions_count(
+        submissions_data, id_to_username, 10)
+
+    longest_submission_data = longest_submission(
+        submissions_data, id_to_username)
+
+    top_failed_submissions_data = top_failed_submissions_by_challenge(
+        submissions_data, hidden_and_admin_user_ids, challenge_name_mapping)
+
+    # output
+    combined_data = {
+        "most_popular_category": most_popular_data,
+        "submissions_per_category": submissions_per_category_data,
+        "solves_per_category": solves_data,
+        "challenge_statistics": challenge_stats_data,
+        "first_bloods": first_bloods_data,
+        "top_users_failed_submissions_count": top_users_failed_data,
+        "longest_submissions": longest_submission_data,
+        "top_failed_submissions_by_challenge": top_failed_submissions_data
+    }
+
+    output_functions = {
+        'txt': output_txt,
+        'json': output_json
+    }
+
+    output_func = output_functions.get(output_format)
+    if output_func:
+        output_func(combined_data)
+    else:
+        logging.error(f"Invalid output format: {output_format}")
 
 
 if __name__ == "__main__":
-    submissions_data = load_json_data('db/submissions.json')
-    users_data = load_json_data('db/users.json')
-    challenges_data = load_json_data('db/challenges.json')
-
-    id_to_username = {user["id"]: user["name"]
-                      for user in users_data.get("results", [])}
-    id_to_score = {user["id"]: 0 for user in users_data.get("results", [])}
-
-    hidden_user_ids, admin_user_ids = get_hidden_and_admin_user_ids(users_data)
-    hidden_and_admin_user_ids = hidden_user_ids | admin_user_ids
-    challenge_category_mapping = get_challenge_category_mapping(challenges_data)
-    challenge_name_mapping = get_challenge_name_mapping(challenges_data)
-
-    rewind()
+    args = parse_args()
+    rewind(args.dir, args.output)
